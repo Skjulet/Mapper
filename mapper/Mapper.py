9382efe079b1daf6a0697191c88b05a67f27e848
Gabriel Isheden <gisheden@csc.kth.se>
2013-06-19 21:13:27 +0200

    Updated all code in Mapper.py after the style, except for the function names

--- mapper/Mapper.py
+++ mapper/Mapper.py
@@ -14,8 +14,8 @@ import Grapher as gr
 
 
 class Mapper:
-    def __init__(self,PointCloud_npArray,Metric_me,Lens_str,BINS_int,
-                OVERLAP_flt,Clust_str,EPS_flt,DebugMode_bol = False):
+    def __init__(self, PointCloud_npArray, Metric_me, Lens_str, BINS_int,
+                OVERLAP_flt, Clust_str, EPS_flt, DebugMode_bol = False):
         '''The Mapper object is initiated with a set PointCloud_npArray and
         dependes on several other variables (see above)
         '''
@@ -33,25 +33,31 @@ class Mapper:
 
         self.Equalize_bol = True
         self.OVERLAP_flt = OVERLAP_flt
-        self.BINS_int = bi.Bins(BINS_int, self.OVERLAP_flt, self.Equalize_bol, self.DebugMode_bol)	
-        self.Lens_str = le.Lens(Lens_str,self.Metric_me,self.BINS_int,DebugMode_bol)
+        self.BINS_int = bi.Bins(BINS_int, self.OVERLAP_flt,
+                                self.Equalize_bol, self.DebugMode_bol)	
+        self.Lens_str = le.Lens(Lens_str, self.Metric_me,
+                                self.BINS_int, DebugMode_bol)
 
         self.EPS_flt = EPS_flt
-        self.Clust_str = cl.Clust(self.PointCloud_npArray,Clust_str,self.EPS_flt,self.Metric_me,self.DebugMode_bol)
+        self.Clust_str = cl.Clust(self.PointCloud_npArray, 
+                                Clust_str, self.EPS_flt, 
+                                self.Metric_me, self.DebugMode_bol)
 
-        self.coloring = None
-        self.grapherobject = None
+        self.Coloring_npArray = None
+        self.GrapherObject_gr = None
         self.properties = None
-        self.labels = None
+        self.Labels_npArray = None
 
         #Filters cloud on the lenses filter
         self.filteredcloud = self.Lens_str.filtered(self.PointCloud_npArray)
         if self.DebugMode_bol == True:
-             print("In Mapper__init__: Printing self.filteredcloud in the Mapper object:")
+             print("In Mapper__init__: Printing self.filteredcloud in the\
+             Mapper object:")
              print(self.filteredcloud)	
 
         #clusters the cloud
-        self.clusteredcloud = self.Clust_str.TESTmakeclustering(self.filteredcloud)
+        self.clusteredcloud = \
+        self.Clust_str.TESTmakeclustering(self.filteredcloud)
 
 
         if self.DebugMode_bol == True:
@@ -60,77 +66,83 @@ class Mapper:
 
 
         #creates a graph from the self.PointCloud_npArray and clustering data
-        self.grapherobject = gr.Grapher(self.PointCloud_npArray,self.clusteredcloud,self.DebugMode_bol)
-        self.grapherobject.makegraph()
+        self.GrapherObject_gr = gr.Grapher(self.PointCloud_npArray, 
+                                    self.clusteredcloud, self.DebugMode_bol)
+        self.GrapherObject_gr.makegraph()
 
-    def mmetric(self,Metric_me):
+    def mmetric(self, Metric_me):
+        '''Function that modifies the self.Metric_me object
         '''
-        '''
 
 
-        self.Metric_me = me.Metric(Metric_me,self.DebugMode_bol)
+        self.Metric_me = me.Metric(Metric_me, self.DebugMode_bol)
 
     def mlens(self, Lens_str):
-        '''
+        '''Function that modifies the self.Lens_str object
         '''
 
 
-        self.Lens_str = le.Lens(Lens_str,self.Metric_me,self.BINS_int,self.DebugMode_bol)
+        self.Lens_str = le.Lens(Lens_str, self.Metric_me,
+                                self.BINS_int, self.DebugMode_bol)
 
-    def moverlap(self,OVERLAP_flt):
-        '''
+    def moverlap(self, OVERLAP_flt):
+        '''Function that modifies the self.OVERLAP_flt object
         '''
 
 
         self.OVERLAP_flt = OVERLAP_flt
 
-    def mbins(self,BINS_int):
-        '''
+    def mbins(self, BINS_int):
+        '''Function that modifies the self.BINS_int object
         '''
 
 
         self.BINS_int = BINS_int
 
-    def mclust(self,Clust_str):
-        '''
-        '''
-
-
-        self.Clust_str = cl.Clust(PointCloud_npArray,Clust_str,self.Metric_me,self.DebugMode_bol)
-
-    def addlabels(self,labels):
-        '''
+    def mclust(self, Clust_str):
+        '''Function that modifies the self.Clust_str object
         '''
 
 
-        self.labels = labels
-        self.grapherobject.addlabels(self.labels)
-        G = self.grapherobject.GetGraph()
+        self.Clust_str = cl.Clust(PointCloud_npArray, Clust_str,
+                                self.Metric_me, self.DebugMode_bol)
+
+    def addlabels(self, Labels_npArray):
+        '''Function that adds Labels_npArray to the graph in
+        self.GrapherObject_gr
+        '''
+
+
+        self.Labels_npArray = Labels_npArray
+        self.GrapherObject_gr.addlabels(self.Labels_npArray)
+        G = self.GrapherObject_gr.GetGraph()
         print(G.nodes(data=True)[0])
         print(G.edges(data=True)[0:5])
 
     def AddFilterToGraph(self):
-        '''
+        '''Function that adds meaned filter values to the nodes in the graph
+        in self.GrapherObject_gr
         '''
 
 
         pass
 
-    def addproperties(self,properties,PropertiesName):
-        '''
+    def addproperties(self, properties, PropertiesName_str):
+        '''Function that adds meaned properties values to the nodes in the
+        graph in self.GrapherObject_gr
         '''
 
 
         self.properties = np.column_stack((self.properties, properties))
 
-    #This function saves the graph in graph_files/ in .graphml format
     def visualize(self):
-        '''
+        '''This function saves the graph in a map in .graphml format
         '''
 
 
         pass
-	#nx.write_graphml(self.grapherobject.GetGraph(), 'graph_files/' + 'TestGraph' + '.graphml')
+	#nx.write_graphml(self.GrapherObject_gr.GetGraph(), 'graph_files/' +\
+	#'TestGraph' + '.graphml')
 
 
 
