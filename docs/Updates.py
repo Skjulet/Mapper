52269ba7d6fd93a046bed560991cc083dd410197
Gabriel Isheden <Gabriel.Isheden@gmail.com>
2013-06-19 23:37:28 +0300

    Update Updates.py

--- docs/Updates.py
+++ docs/Updates.py
@@ -5,6 +5,7 @@ Minor changes:
 19/6 2013 - Added clustering array in TESTgrapher in Grapher object
 19/6 2013 - Added FilterFunctions.py in the map Filter Functions
 19/6 2013 - Changed the Grapher class so that labels are added after the graph is created
+19/6 2013 - Created package structure out of the project
 
 Major changes:
 '''
