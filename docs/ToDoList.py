110341bcc395253d8d55a6fdd4a8f6064109c25a
Gabriel Isheden <Gabriel.Isheden@gmail.com>
2013-06-20 17:27:17 +0200

    Updated Filters.py according to style guidelines. Updated ToDoList.py and Updates.py

--- docs/ToDoList.py
+++ docs/ToDoList.py
@@ -2,29 +2,34 @@
 Work to be done:
 
 Short term:
-(H) Rewrite all code according to the style guide in README.md!!
-(*) ADD COMMENTS TO APPROPIATE WORK
-
-(M) INSERTION OF other PROPERTIES
+    (H) ArielArielAriel ArielAriel Ariel --> fixa variabelnamn i 
+        Filters.py.
+    (H) Rewrite all code according to the style guide in README.md!!
+    (H) Filter functions from Mapper/mapper/Filter_Functions linked to
+        Filters.py.
+        
+    (M) INSERTION OF other PROPERTIES.
+    
+    (*) ADD COMMENTS TO APPROPIATE WORK.
 
 
 Middle term:
-(M) Fix equalize == false
-(L) Separate clusterdata, binningdata etc
-(L) incorporate graphing + clustering into binning
-(H) Filter functions
-(M/L) Clustering algorithms
-(M) Appropriate way to handle clusterings and filters in the code
-(H) Create python package ofthe project
+    (M) Appropriate way to handle clusterings and filters in the code
+    (M) Fix equalize == false in Bins.py.
+    (M/L) Clustering algorithms
+    
+    (L) Separate clusterdata, binningdata etc.
+    (L) incorporate graphing + clustering into binning    
+    
 
 Long term:
-(H) Implement a user interface
-(H) save calculation heavy data in file or RAM etc.
+    (H) Implement a user interface
+    (H) save calculation heavy data in file or RAM etc.
+    (H) Save project file/session with configurations
 
-(H) Save project file/session with configurations
-
-(M) Parrallellisering
-(*) Optimize parameter choice (epsilons,bins,overlap, etc.)
-(*) Support for big files (?)
+    (M) Parrallellisering
+    
+    (*) Optimize parameter choice (epsilons,bins,overlap, etc.)
+    (*) Support for big files (?)
 
 '''
